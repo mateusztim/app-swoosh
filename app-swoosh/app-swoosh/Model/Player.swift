05120a7c5688b7b8866a02bce9cce2931a0db823
Mateusz Tim <mateusztim@MacBook-Pro.local>
2017-08-21 12:18:21 +0200

    struct and func

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Mateusz Tim on 21.08.2017.
+//  Copyright © 2017 Mateusz Tim. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
